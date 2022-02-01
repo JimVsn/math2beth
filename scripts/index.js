0cb89c7dd8038277d2d15596a8b26018e247aba4
JimVsn <juarvasdi@yandex.ru>
2022-02-01 12:33:19 +0300

    feat:раскоммент, округление

--- scripts/index.js
+++ scripts/index.js
@@ -85,38 +85,38 @@ if(u===1)
    y=y*1.8;
   
   
-  //  if (w===4) m=m+(1*2); y=y-(1*2);
+   if (w===4) m=m+(1*2); y=y-(1*2);
   
-  //  if (w===3) m=m+(1*1.5); y=y-(1*1.5);
+   if (w===3) m=m+(1*1.5); y=y-(1*1.5);
    
-  //  if (w===2) m=m+(1*1); y=y-(1*1);
+   if (w===2) m=m+(1*1); y=y-(1*1);
    
-  //  if (w===1) m=m+(1*0.8); y=y-(1*0.8);
+   if (w===1) m=m+(1*0.8); y=y-(1*0.8);
    
-  //  if (w===-1) m=m-(1*0.8); y=y+(1*0.8);
+   if (w===-1) m=m-(1*0.8); y=y+(1*0.8);
    
-  //  if (w===-2) m=m-(1*1); y=y+(1*1);
+   if (w===-2) m=m-(1*1); y=y+(1*1);
   
-  //  if (w===-3) m=m-(1*1.5); y=y+(1*1.5);
+   if (w===-3) m=m-(1*1.5); y=y+(1*1.5);
  
-  //  if (w===-4) m=m-(1*2); y=y+(1*2);
+   if (w===-4) m=m-(1*2); y=y+(1*2);
   
  
-  // if (w===4) y=y-(1*2);
+  if (w===4) y=y-(1*2);
  
-  // if (w===3) y=y-(1*1.5);
+  if (w===3) y=y-(1*1.5);
   
-  // if (w===2) y=y-(1*1);
+  if (w===2) y=y-(1*1);
  
-  // if (w===1) y=y-(1*0.8);
+  if (w===1) y=y-(1*0.8);
  
-  // if (w===-1) y=y+(1*0.8);
+  if (w===-1) y=y+(1*0.8);
  
-  // if (w===-2) y=y+(1*1);
+  if (w===-2) y=y+(1*1);
  
-  // if (w===-3) y=y+(1*1.5);
+  if (w===-3) y=y+(1*1.5);
  
-  // if (w===-4) y=y+(1*2);
+  if (w===-4) y=y+(1*2);
   
 
 
@@ -125,13 +125,12 @@ if(u===1)
  
   if (y<0.11) y=0.01;
 
-  popupResultHome.textContent = m;
-  popupResultAway.textContent = y;
+  popupResultHome.textContent = m.toFixed(1);
+  popupResultAway.textContent = y.toFixed(1);
 }
 
 function openPopup(popup){
   popup.classList.add('popup_opened');
-//   document.addEventListener('keydown', popupCloseEscape);
 }
 
 function closePopup(popup) {
@@ -149,135 +148,3 @@ popupMathRun.addEventListener('click', () => {
 
 
 
-// {/* <script type="text/javascript">
-
-
-// <script type="text/javascript">
-
-
-// function areaRectangle(){
-//   var a=1*document.forma1.t1.value;
-
-//   var b=1*document.forma1.t2.value;
-
-//   var e=1*document.forma1.t3.value;
-
-//   var o=1*document.forma1.t4.value;
-
-//   var p=1*document.forma1.t5.value;
-
-//   var q=1*document.forma1.t6.value;
-
-//   var d=1*document.forma1.t7.value;
-
-//   var f=1*document.forma1.t8.value;
-
-//   var j=1*document.forma1.t9.value;
-
-//   var k=1*document.forma1.t10.value;
-
-
-
-//   var l=(a/3+o/3)-1.2;
-
-//   var r=(b/3+e/3)-1.2;
-
-//   var u=p-f;
-
-//   var i=q-d;
-
-//   var w=j-k;
-
-//   var m=l;
-
-//   var y=r;
-
-
-
-//  var m=m+(0);
-//  var y=y-(0);
-
-  
-// if(u===1)
-//   m=m*0.8;
-
-//   if(u===2)
-//    m=m*0.5;
-
-//   if(u===3) 
-//    m=m*0.1;
-
-//   if(u===-1)
-//   m=m*1.1;
-
-//   if(u===-2)
-//    m=m*1.5;
- 
-//   if(u===-3) 
-//    m=m*1.8;
-
-  
-//   if(i===1) 
-//   y=y*0.8;
-  
-//   if(i===2) 
-//   y=y*0.5;
-  
-//   if(i===3) 
-//   y=y*0.1;
-  
-//   if(i===-1) 
-//   y=y*1.1;
-  
-//   if(i===-2) 
-//   y=y*1.5;
-  
-//   if(i===-3) 
-//   y=y*1.8;
-  
-  
-//   if (w===4) m=m+(1*2); y=y-(1*2);
-  
-//   if (w===3) m=m+(1*1.5); y=y-(1*1.5);
-  
-//   if (w===2) m=m+(1*1); y=y-(1*1);
-  
-//   if (w===1) m=m+(1*0.8); y=y-(1*0.8);
-  
-//   if (w===-1) m=m-(1*0.8); y=y+(1*0.8);
-  
-//   if (w===-2) m=m-(1*1); y=y+(1*1);
- 
-//   if (w===-3) m=m-(1*1.5); y=y+(1*1.5);
-
-//   if (w===-4) m=m-(1*2); y=y+(1*2);
-  
- 
-//   if (w===4) y=y-(1*2);
- 
-//   if (w===3) y=y-(1*1.5);
-  
-//   if (w===2) y=y-(1*1);
- 
-//   if (w===1) y=y-(1*0.8);
- 
-//   if (w===-1) y=y+(1*0.8);
- 
-//   if (w===-2) y=y+(1*1);
- 
-//   if (w===-3) y=y+(1*1.5);
- 
-//   if (w===-4) y=y+(1*2);
-  
-
-
-
-//   if (m<0.11) m=0.01;
- 
-//   if (y<0.11) y=0.01;
-
-//   document.forma1.res.value=m;
-//   document.forma1.res2.value=y;
-// }
-
-//  </script> */}
